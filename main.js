bea7046b89395c689db0d5c622486e7ea88e727b
Bry Denney <bry@Squirrel.(none)>
2014-03-15 01:51:23 -0500

    changing the insult for being lazy and using shorter magic8ball command variant

--- main.js
+++ main.js
@@ -20,26 +20,26 @@ var bot = new irc.Client(config.server, config.botName, {
 });
 
 var magic8ballAnswers = [
-"It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes definitely",
-"You may rely on it",
-"As I see it, yes",
-"Most likely",
-"Outlook good",
-"Yes",
-"Signs point to yes",
-"Reply hazy try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
-"Don't count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good",
-"Very doubtful"
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
 ];
 
 //main
@@ -113,12 +113,17 @@ function addListeners() {
       {
         botString = botString + ", assface.";
       }
+      else
+      {
+        botString = botString + ".";
+      }
       
-      bot.say(ps1Channel, botString);
       if(lazy)
       {
-        bot.say(ps1Channel, "Y'all are lazy.");
+        botString = botString + " (you must have saved so much time not typing 5 characters)";
       }
+
+      bot.say(ps1Channel, botString);
       return;
     }
 
