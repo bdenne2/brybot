7fb04e87cbd38545ebf6cdb15a092473bc1e6591
Bry Denney <bry@Squirrel.(none)>
2014-03-15 20:36:11 -0500

    once again, correcting channel

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 //config
-var ps1Channel = "#pumpingstationone.org";
+var ps1Channel = "#pumpingstationone";
 var config = {
   channels: [ps1Channel],
   server: "chat.freenode.net",
